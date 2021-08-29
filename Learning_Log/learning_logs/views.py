da8de319ab4a2a1e4f8b0ff38a438819d2237a34
Adam Wawrzyniak <adamwawrzyniak5@gmail.com>
2021-08-29 21:13:38 +0200

    Dod

--- Learning_Log/learning_logs/views.py
+++ Learning_Log/learning_logs/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
@@ -79,3 +80,22 @@ def edit_entry(request, entry_id):
     #Wyświetlanie pustego formularza.
     context = {'entry':entry, 'topic':topic, 'form': form}
     return render(request, 'learning_logs/edit_entry.html', context)
+
+def register(request):
+    """Rejestracja nowego użytkownika"""
+    if request.method != 'POST':
+        #Wyświetlanie pustego formularza rejestracji użytkownika
+        form = UserCreationForm()
+    else:
+        #Przetworzenie wypełnionego formularza.
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            #Zalogowanie użytkownika, a następnie przekierowanie go na stronę
+            # główną.
+            login(request, new_user)
+            return redirect('learning_logs:index')
+
+    #Wyświetlanie pustego formularza.
+    context = {'form':form}
+    return render(request, 'registaration/register.html', context)
