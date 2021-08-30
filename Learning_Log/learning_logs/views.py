bc75275bdb450925e8159f6fc69fedd73e6bdb98
Adam Wawrzyniak <adamwawrzyniak5@gmail.com>
2021-08-30 21:22:18 +0200

    _

--- Learning_Log/learning_logs/views.py
+++ Learning_Log/learning_logs/views.py
@@ -17,6 +17,7 @@ def topics(request):
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     """Wyświetla pojędyńczy teamt i wszystkie powiązane z nim wpisy."""
     topic = Topic.objects.get(id=topic_id)
@@ -24,6 +25,7 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     """Dodaj nowy temat."""
     if request.method != 'POST':
@@ -40,6 +42,7 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     """Dodanie nowego wpisu dla określonego tematu."""
     topic = Topic.objects.get(id=topic_id)
@@ -61,6 +64,7 @@ def new_entry(request, topic_id):
     context = {'topic':topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """Edycja instniejącego wpisu."""
     entry = Entry.objects.get(id=entry_id)
