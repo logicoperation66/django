01ab678ddee86ecafed738fc22e6725ebd94059c
Adam Wawrzyniak <adamwawrzyniak5@gmail.com>
2021-09-22 22:54:36 +0200

    Dodanie appki bootstrapa

--- Learning_Log/learning_log/settings.py
+++ Learning_Log/learning_log/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'learning_logs',
     'users',
 
+    #Aplikacje innych firm.
+    'bootstrap4',
+
     #Domyślne aplikacje Django
     'django.contrib.admin',
     'django.contrib.auth',
